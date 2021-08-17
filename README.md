6338f1d0e1d1a3ca92cfe78fd5e897010ae6b96e
Michael Ripley <zkxs00@gmail.com>
2021-08-17 16:59:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# SsaoDisable
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that disables all SSAO post processing.
+
+This will no longer be needed once https://github.com/Neos-Metaverse/NeosPublic/issues/322 is resolved.
+
+Credit for this idea goes to [NeosWithoutSsao])https://github.com/hiinaspace/NeosWithoutSsao) v1.0.
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [SsaoDisable.dll](https://github.com/zkxs/SsaoDisable/releases/latest/download/SsaoDisable.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+
+## FAQ
+### What does this actually do?
+It force disables the Unity SSAO post processing effect for all cameras by replacing the camera setup method with a slightly modified version.
+
+### Is this against guidelines?
+Maybe. [Read more](https://github.com/zkxs/NeosModLoader/blob/master/doc/neos_guidelines.md).
---
+++ SsaoDisable/SsaoDisable.cs
@@ -0,0 +1,73 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace SsaoDisable
+{
+    public class SsaoDisable : NeosMod
+    {
+        public override string Name => "SsaoDisable";
+        public override string Author => "runtime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/zkxs/SsaoDisable";
+
+        private static bool _first_trigger = false;
+
+        public override void OnEngineInit()
+        {
+            // disable future SSAO by patching the setup method
+            Harmony harmony = new Harmony("net.michaelripley.SsaoDisable");
+            MethodInfo originalMethod = AccessTools.DeclaredMethod(typeof(CameraInitializer), nameof(CameraInitializer.SetPostProcessing), new Type[] { typeof(Camera), typeof(bool), typeof(bool), typeof(bool) });
+            if (originalMethod == null)
+            {
+                Error("Could not find CameraInitializer.SetPostProcessing(Camera, bool, bool, bool)");
+                return;
+            }
+            MethodInfo replacementMethod = AccessTools.DeclaredMethod(typeof(SsaoDisable), nameof(SetPostProcessingPostfix));
+            harmony.Patch(originalMethod, postfix: new HarmonyMethod(replacementMethod));
+            Msg("Hook installed successfully");
+
+            // disable prexisting SSAO by searching for all matching Unity components
+            AmplifyOcclusionEffect[] components = Resources.FindObjectsOfTypeAll<AmplifyOcclusionEffect>();
+            int count = 0;
+            foreach (AmplifyOcclusionEffect ssao in components)
+            {
+                try
+                {
+                    ssao.enabled = false;
+                    count += 1;
+                }
+                catch(Exception e)
+                {
+                    Warn($"failed to disable a prexisting SSAO: {e}");
+                }
+            }
+            Msg($"disabled {count} prexisting SSAOs");
+        }
+
+        private static void SetPostProcessingPostfix(Camera c, bool enabled, bool Ssao, bool screenspaceReflections)
+        {
+            try
+            {
+                AmplifyOcclusionEffect ssao = c.GetComponent<AmplifyOcclusionEffect>();
+                if (ssao != null)
+                {
+                    ssao.enabled = false;
+
+                    if (!_first_trigger)
+                    {
+                        _first_trigger = true;
+                        Msg("Hook triggered! Everything worked!");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Warn($"failed to disable a new SSAO: {e}");
+            }
+        }
+    }
+}
